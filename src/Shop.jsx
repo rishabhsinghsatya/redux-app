26af57a6e1003d76cd43ccb2891b52660113193c
Rishabh Singh <vineetsatya36@gmail.com>
2024-04-12 19:23:04 +0530

    setup

---
+++ src/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h4>Shoes</h4>
+      <button className="btn btn-primary">-</button>Add to cart
+      <button className="btn btn-primary">+</button>
+    </div>
+  );
+};
+
+export default Shop;
